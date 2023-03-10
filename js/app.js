957f742fcb5520c281259f439a754d89446e8277
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-03-10 15:27:51 -0800

    question5

--- js/app.js
+++ js/app.js
@@ -73,14 +73,14 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testArray = [2, 3, 4];
 
-function sumArray(){
-  console.log(sum(testArray[0], testArray[1], testArray[2]), testArray + ' was passed in as an array of numbers, and ' + sum(testArray[0], testArray[1], testArray[2]) + ' is their sum.');
-  return [parseInt(sum(testArray[0], testArray[1], testArray[2])), testArray + ' was passed in as an array of numbers, and ' + sum(testArray[0], testArray[1], testArray[2]) + ' is their sum.'];
-}
+// function sumArray(){
+//   console.log(sum(testArray[0], testArray[1], testArray[2]), testArray + ' was passed in as an array of numbers, and ' + sum(testArray[0], testArray[1], testArray[2]) + ' is their sum.');
+//   return [parseInt(sum(testArray[0], testArray[1], testArray[2])), testArray + ' was passed in as an array of numbers, and ' + sum(testArray[0], testArray[1], testArray[2]) + ' is their sum.'];
+// }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+// testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -95,12 +95,13 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
-
+function multiplyArray(testArray) {
+  console.log(multiply(testArray[0], testArray[1], testArray[2]), 'The numbers ' + testArray + ' have a product of ' + multiply(testArray[0], testArray[1], testArray[2]) + '.');
+  return [parseInt(multiply(testArray[0], testArray[1], testArray[2])), 'The numbers ' + testArray + ' have a product of ' + multiply(testArray[0], testArray[1], testArray[2]) + '.'];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
