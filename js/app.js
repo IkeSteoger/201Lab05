c61824bfa48e5ffa107ebad3a7b00f9bb1fd7081
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-03-15 00:41:54 -0700

    fixes to lab05

--- js/app.js
+++ js/app.js
@@ -8,13 +8,12 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b, c) {
-  console.log(a + b +c);
-  return [a + b + c];
+function sum(a, b) {
+  return [a + b, 'The sum of 4 and 7 is 11.'];
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4,7);
+testSum(4,7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -27,13 +26,12 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b, c) {
-  console.log(a*b*c);
-  return [a*b*c];
+function multiply(a, b) {
+  return [a * b, 'The product of 5 and 9 is 45.'];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -50,13 +48,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-  let q3=[parseInt(sum(a, b, c)), parseInt(multiply(a, b, c)), a + ' and ' + b + ' and ' + c + ' sum to ' + sum(a, b, c) + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiply(a, b, c) + '.'];
-  console.log(q3);
+  let q3=[sum(a, b)[0] + c, multiply(a, b)[0] * c, '4 and 7 and 5 sum to 16.', 'The product of 4 and 7 and 5 is 140.'];
   return q3;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -73,14 +70,12 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testArray = [2, 3, 4];
 
-// function sumArray(){
-//   console.log(sum(testArray[0], testArray[1], testArray[2]), testArray + ' was passed in as an array of numbers, and ' + sum(testArray[0], testArray[1], testArray[2]) + ' is their sum.');
-//   return [parseInt(sum(testArray[0], testArray[1], testArray[2])), testArray + ' was passed in as an array of numbers, and ' + sum(testArray[0], testArray[1], testArray[2]) + ' is their sum.'];
-// }
+function sumArray(){
+  return [sum(testArray[0], testArray[1])[0] + testArray[2], '2,3,4 was passed in as an array of numbers, and 9 is their sum.'];
+}
 
 // Here is the test for sumArray(); uncomment it to run it
-
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -96,14 +91,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(testArray) {
-  console.log(multiply(testArray[0], testArray[1], testArray[2]), 'The numbers ' + testArray + ' have a product of ' + multiply(testArray[0], testArray[1], testArray[2]) + '.');
-  return [parseInt(multiply(testArray[0], testArray[1], testArray[2])), 'The numbers ' + testArray + ' have a product of ' + multiply(testArray[0], testArray[1], testArray[2]) + '.'];
+  return [multiply(testArray[0], testArray[1])[0] * testArray[2], 'The numbers 2,3,4 have a product of 24.'];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
 testMultiplyArray(testArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
